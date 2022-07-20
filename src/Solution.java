c92d35e2c3d35e65f92f967ef1c8e1a47654f4a3
Dmitry Shishkin <4144442@bk.ru>
2022-07-20 18:18:08 +0300

    Add Two Numbers

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,70 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] nums = {2, 7, 11, 15};
-        Arrays.stream(solution.twoSum(nums, 18)).forEach(System.out::println);
+    public static class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 
-    public int[] twoSum(int[] nums, int target) {
-        int[] numsClone = nums.clone();
-        Arrays.sort(numsClone);
-        for (int i = 0; i < nums.length; i++) {
-            int first = nums[i];
-            int second = target - first;
-            int key = Arrays.binarySearch(numsClone, second);
-            if (key >= 0) {
-                for (int j = 0; j < nums.length; j++) {
-                    if (nums[j] == second && i != j)
-                        return new int[]{i, j};
-                }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode node1 = new ListNode(9);
+        ListNode node2 = new ListNode(9, node1);
+        ListNode node3 = new ListNode(9, node2);
+        ListNode node4 = new ListNode(9,node3);
+        ListNode node5 = new ListNode(9);
+        ListNode node6 = new ListNode(9, node5);
+        System.out.println(solution.addTwoNumbers(node4, node6).next.next.next.next.val);
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode zero = new ListNode(0);
+        if (l1 == null && l2 == null)
+            return zero;
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        int one = 0;
+        List<Integer> list = new ArrayList<>();
+        while (l1 != null || l2 != null) {
+            int a = 0;
+            int b = 0;
+            if (l1 != null) {
+                a = l1.val;
+                l1 = l1.next;
             }
+            if (l2 != null) {
+                b = l2.val;
+                l2 = l2.next;
+            }
+            int sum = a + b + one;
+            one = (sum < 10) ? 0 : 1;
+            list.add(sum % 10);
         }
-        return null;
+        if(one==1)
+            list.add(1);
+        ListNode nextNode = new ListNode(list.get(list.size() - 1));
+        ListNode node = null;
+        for (int i = list.size() - 2; i >= 0; i--) {
+            node = new ListNode(list.get(i), nextNode);
+            nextNode = node;
+        }
+        return nextNode;
     }
 }
