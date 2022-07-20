e668aa91d678363150603d1a7db2777564afe93d
Dmitry Shishkin <4144442@bk.ru>
2022-07-20 15:26:25 +0300

    Две суммы

--- src/Solution.java
+++ src/Solution.java
@@ -4,12 +4,24 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[][] accounts = {{1, 5}, {7, 3}, {3, 5}};
-        System.out.println(solution.maximumWealth(accounts));
+        int[] nums = {2, 7, 11, 15};
+        Arrays.stream(solution.twoSum(nums, 18)).forEach(System.out::println);
     }
 
-    public int maximumWealth(int[][] accounts) {
-        return Arrays.stream(accounts).mapToInt(acc -> Arrays.stream(acc).reduce(Integer::sum).orElse(0))
-                .max().orElse(0);
+    public int[] twoSum(int[] nums, int target) {
+        int[] numsClone = nums.clone();
+        Arrays.sort(numsClone);
+        for (int i = 0; i < nums.length; i++) {
+            int first = nums[i];
+            int second = target - first;
+            int key = Arrays.binarySearch(numsClone, second);
+            if (key >= 0) {
+                for (int j = 0; j < nums.length; j++) {
+                    if (nums[j] == second && i != j)
+                        return new int[]{i, j};
+                }
+            }
+        }
+        return null;
     }
 }
