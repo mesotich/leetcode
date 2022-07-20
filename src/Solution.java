c2935b3052f58088eefa63006d41327a299c7d49
Dmitry Shishkin <4144442@bk.ru>
2022-07-20 14:03:34 +0300

    Самый богатый человек

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,15 @@
+import java.util.Arrays;
+
 class Solution {
 
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] accounts = {{1, 5}, {7, 3}, {3, 5}};
+        System.out.println(solution.maximumWealth(accounts));
+    }
+
+    public int maximumWealth(int[][] accounts) {
+        return Arrays.stream(accounts).mapToInt(acc -> Arrays.stream(acc).reduce(Integer::sum).orElse(0))
+                .max().orElse(0);
+    }
 }
