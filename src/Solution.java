ff9264334e81c3ce09036bdcec3506bee70e05a9
Dmitry Shishkin <4144442@bk.ru>
2022-07-21 18:34:39 +0300

    7. Reverse Integer

--- src/Solution.java
+++ src/Solution.java
@@ -2,28 +2,20 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.convert("ABCDE", 4));
+        System.out.println(solution.reverse(1534236469));
+        System.out.println(solution.reverse(-2147483412));
     }
 
-    public String convert(String s, int numRows) {
-        if (numRows == 1)
-            return s;
-        StringBuilder sb = new StringBuilder();
-        int left;
-        int right;
-        int step = 2 * (numRows - 1);
-        int column = s.length() / step + 2;
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < column; j++) {
-                left = j * step - i;
-                right = j * step + i;
-                if (left >= 0 && left < s.length() && left != j * step - step / 2 && left != right)
-                    sb.append(s.charAt(left));
-                if (right >= 0 && right < s.length())
-                    sb.append(s.charAt(right));
-            }
+    public int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            if (result > Integer.MAX_VALUE / 10 || result < Integer.MIN_VALUE / 10)
+                return 0;
+            result *= 10;
+            result += x % 10;
+            x /= 10;
         }
-        return sb.toString();
+        return result;
     }
 }
 
