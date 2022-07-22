9e097b28689d106f91265513b71967e0b34a9413
Dmitry Shishkin <4144442@bk.ru>
2022-07-22 15:05:47 +0300

    8. String to Integer (atoi)

--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,45 @@
 class Solution {
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.reverse(1534236469));
-        System.out.println(solution.reverse(-2147483412));
-    }
-
-    public int reverse(int x) {
-        int result = 0;
-        while (x != 0) {
-            if (result > Integer.MAX_VALUE / 10 || result < Integer.MIN_VALUE / 10)
+    public int myAtoi(String s) {
+        if (s == null || s.isEmpty())
+            return 0;
+        int len = s.length();
+        char ch = s.charAt(0);
+        if (len == 1 && ch < 48 || ch > 59)
+            return 0;
+        int start = 0;
+        int end;
+        boolean isSignFind = false;
+        boolean isMinus = false;
+        int result;
+        while (((ch = s.charAt(start)) == ' ' && !isSignFind) || ch == '0'
+                || (!isSignFind && ch == '+') || (!isSignFind && ch == '-')) {
+            if (ch == '0' || ch == '+' || ch == '-') {
+                isSignFind = true;
+                if (ch == '-')
+                    isMinus = true;
+            }
+            start++;
+            if (start == len)
                 return 0;
-            result *= 10;
-            result += x % 10;
-            x /= 10;
         }
-        return result;
+        if (ch < 48 || ch > 57)
+            return 0;
+        end = start;
+        while ((ch = s.charAt(end)) >= 48 && ch <= 57) {
+            if (end - start >= 10)
+                return isMinus ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            end++;
+            if (end == len)
+                break;
+        }
+        try {
+            result = Integer.parseInt(s.substring(start, end));
+        } catch (NumberFormatException e) {
+            return isMinus ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        }
+        return isMinus ? -result : result;
     }
 }
+
 
