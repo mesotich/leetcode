80b1488d209c70e9c4502587cc9d60de84679c99
Dmitry Shishkin <4144442@bk.ru>
2022-07-24 23:06:06 +0300

    11. Container With Most Water

--- src/Solution.java
+++ src/Solution.java
@@ -2,28 +2,20 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.isPalindrome(1));
+        System.out.println(solution.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
     }
 
-    public boolean isPalindrome(int x) {
-        if (x == 0)
-            return true;
-        if (x < 0)
-            return false;
-        int num = (int) Math.log10(x);
-        if (num == 0)
-            return true;
-        int del;
-        while (num >= 1) {
-            del = (int) Math.pow(10, num);
-            if (x % 10 != x / del) {
-                return false;
-            }
-            x = x % del;
-            x /= 10;
-            num -= 2;
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxSquare = 0;
+        int min = 0;
+        while (left < right) {
+            min = Math.min(height[left], height[right]);
+            maxSquare = Math.max(maxSquare, (right - left) * min);
+            min = min == height[left] ? left++ : right--;
         }
-        return true;
+        return maxSquare;
     }
 }
 
