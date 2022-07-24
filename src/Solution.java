fc2cbba88c80b4bbd0d7d05befa4656ee719e5bc
Dmitry Shishkin <4144442@bk.ru>
2022-07-24 17:56:00 +0300

    10. Regular_Expression_Matching

--- src/Solution.java
+++ src/Solution.java
@@ -2,28 +2,42 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.isPalindrome(1));
+        System.out.println(solution.isMatch("a", "a"));
     }
 
-    public boolean isPalindrome(int x) {
-        if (x == 0)
-            return true;
-        if (x < 0)
-            return false;
-        int num = (int) Math.log10(x);
-        if (num == 0)
-            return true;
-        int del;
-        while (num >= 1) {
-            del = (int) Math.pow(10, num);
-            if (x % 10 != x / del) {
-                return false;
+    public boolean isMatch(String s, String p) {
+        int sLen = s.length();
+        int pLen = p.length();
+        int sI = 0;
+        int pI = 0;
+        char sCh = 0;
+        char pCh = 0;
+        boolean sStop = false;
+        boolean pStop = false;
+        boolean isDiff = false;
+        while (sI < sLen || pI < pLen) {
+            if (!sStop && sI < sLen) sCh = s.charAt(sI);
+            if (!pStop && pI < pLen) pCh = p.charAt(pI);
+            if (sCh != pCh && pCh != '.') {
+                if (pCh == '*') {
+                    isDiff = false;
+                    sStop = false;
+                    pCh = p.charAt(pI - 1);
+                    if (pCh == '.') return true;
+                    pStop = sCh == pCh;
+                } else {
+                    if (!isDiff) isDiff = true;
+                    else return false;
+                    if (pStop) pStop = false;
+                }
+            } else {
+                if (!isDiff)
+                    sStop = true;
             }
-            x = x % del;
-            x /= 10;
-            num -= 2;
+            if (sI < sLen && !sStop) sI++;
+            if (pI < pLen && !pStop) pI++;
         }
-        return true;
+        return !isDiff;
     }
 }
 
