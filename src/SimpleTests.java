3a38f965ade7ef08a51c584056d78c962f46feac
Dmitry Shishkin <4144442@bk.ru>
2022-08-01 19:17:52 +0300

    19. Remove Nth Node From End of List

--- src/SimpleTests.java
+++ src/SimpleTests.java
@@ -1,72 +1,57 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.w3c.dom.NodeList;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleTests {
 
-    Solution solution = new Solution();
+    private Solution solution;
+    private NodeList nodeList;
+
+    @Before
+    public void before() {
+        solution = new Solution();
+    }
 
     @Test
     public void testSimple1() {
-        List<List<Integer>> actual = solution.fourSum(new int[]{-2, -1, 0, 0, 1, 2}, 0);
-        List<List<Integer>> expected = List.of(List.of(-2, -1, 1, 2), List.of(-2, 0, 0, 2), List.of(-1, 0, 0, 1));
+        Solution.ListNode node1 = new Solution.ListNode(5, null);
+        Solution.ListNode node2 = new Solution.ListNode(4, node1);
+        Solution.ListNode node3 = new Solution.ListNode(3, node2);
+        Solution.ListNode node4 = new Solution.ListNode(2, node3);
+        Solution.ListNode node5 = new Solution.ListNode(1, node4);
+        Solution.ListNode ac = solution.removeNthFromEnd(node5, 2);
+        List<Integer> actual = new ArrayList<>();
+        while (ac.next != null) {
+            actual.add(ac.val);
+            ac = ac.next;
+        }
+        actual.add(ac.val);
+        List<Integer> expected = new ArrayList<>(List.of(1, 2, 3, 5));
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testSimple2() {
-        List<List<Integer>> actual = solution.fourSum(new int[]{2, 2, 2, 2, 2}, 8);
-        List<List<Integer>> expected = List.of(List.of(2, 2, 2, 2));
-        Assert.assertEquals(expected, actual);
+        Solution.ListNode node1 = new Solution.ListNode(1, null);
+        Solution.ListNode ac = solution.removeNthFromEnd(node1, 1);
+        Assert.assertNull(ac);
     }
-
     @Test
     public void testSimple3() {
-        List<List<Integer>> actual = solution.fourSum(new int[]{2}, 2);
-        List<List<Integer>> expected = Collections.emptyList();
+        Solution.ListNode node1 = new Solution.ListNode(2, null);
+        Solution.ListNode node2 = new Solution.ListNode(1, node1);
+        Solution.ListNode ac = solution.removeNthFromEnd(node2, 1);
+        List<Integer> actual = new ArrayList<>();
+        while (ac.next != null) {
+            actual.add(ac.val);
+            ac = ac.next;
+        }
+        actual.add(ac.val);
+        List<Integer> expected = new ArrayList<>(List.of(1));
         Assert.assertEquals(expected, actual);
     }
-
-    @Test
-    public void testSimple4() {
-        List<List<Integer>> actual = solution.fourSum(new int[]{0}, 0);
-        List<List<Integer>> expected = Collections.emptyList();
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testSimple5() {
-        List<List<Integer>> actual = solution.fourSum(new int[]{0, 1, 2}, 3);
-        List<List<Integer>> expected = Collections.emptyList();
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testSimple6() {
-        List<List<Integer>> actual = solution.fourSum(new int[]{-3, -2, -1, 0, 0, 1, 2, 3}, 0);
-        List<List<Integer>> expected = List.of(List.of(-3, -2, 2, 3), List.of(-3, -1, 1, 3), List.of(-3, 0, 0, 3),
-                List.of(-3, 0, 1, 2), List.of(-2, -1, 0, 3), List.of(-2, -1, 1, 2), List.of(-2, 0, 0, 2), List.of(-1, 0, 0, 1));
-        Assert.assertEquals(expected, actual);
-    }
-    @Test
-    public void testSimple7() {
-        List<List<Integer>> actual = solution.fourSum(new int[]{-2,-1,-1,1,1,2,2}, 0);
-        List<List<Integer>> expected = List.of(List.of(-2,-1,1,2), List.of(-1,-1,1,1));
-        Assert.assertEquals(expected, actual);
-    }
-    @Test
-    public void testSimple8() {
-        List<List<Integer>> actual = solution.fourSum(new int[]{1000000000,1000000000,1000000000,1000000000}, -294967296);
-        List<List<Integer>> expected = Collections.emptyList();
-        Assert.assertEquals(expected, actual);
-    }
-//Input
-//[1000000000,1000000000,1000000000,1000000000]
-//-294967296
-//Output
-//[[1000000000,1000000000,1000000000,1000000000]]
-//Expected
-//[]
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,59 +1,48 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 class Solution {
 
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        if (nums.length < 4)
-            return Collections.emptyList();
-        List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(nums);
-        long sum;
-        for (int first = 0; first < nums.length - 3; first++) {
-            if (first - 1 >= 0 && nums[first] == nums[first - 1])
-                continue;
-            sum = (long) nums[first] + (long) nums[first + 1] + (long) nums[first + 2] + (long) nums[first + 3];
-            if (sum > target)
-                continue;
-            sum = (long) nums[first] + (long) nums[nums.length - 3] + (long) nums[nums.length - 2] + (long) nums[nums.length - 1];
-            if (sum < target)
-                continue;
-            for (int second = first + 1; second < nums.length - 2; second++) {
-                if (second - 1 > first && nums[second] == nums[second - 1])
-                    continue;
-                long twoSum = (long) nums[first] + (long) nums[second];
-                sum = twoSum + (long) nums[second + 1] + (long) nums[second + 2];
-                if (sum > target)
-                    continue;
-                sum = twoSum + (long) nums[nums.length - 2] + (long) nums[nums.length - 1];
-                if (sum < target)
-                    continue;
-                int index = nums.length;
-                for (int third = second + 1; third < nums.length - 1; third++) {
-                    if (third - 1 > second && nums[third] == nums[third - 1])
-                        continue;
-                    long threeSum = twoSum + (long) nums[third];
-                    sum = threeSum + (long) nums[third + 1];
-                    if (sum > target)
-                        continue;
-                    sum = threeSum + (long) nums[nums.length - 1];
-                    if (sum < target)
-                        continue;
-                    if (index <= third + 1)
-                        index = nums.length;
-                    index = Arrays.binarySearch(nums, third + 1, index, (int) (target - threeSum));
-                    if (index >= 0) {
-                        result.add(new ArrayList<>(List.of(nums[first], nums[second], nums[third], nums[index])));
-                    } else index = ~index;
-                }
-            }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head.next == null) return null;
+        ListNode tail = head;
+        for (int i = 1; i < n; i++) {
+            tail = tail.next;
         }
-        return result;
+        if (tail.next == null) {
+            head = head.next;
+            return head;
+        }
+        tail = tail.next;
+        ListNode delNode = head.next;
+        ListNode nextNode = delNode.next;
+        ListNode previousNode = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            delNode = delNode.next;
+            previousNode = previousNode.next;
+            nextNode = nextNode.next;
+        }
+        previousNode.next = nextNode;
+        return head;
+    }
+
+    public static class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 }
-
 
 
 
