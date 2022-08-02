9ea6c63520829f9d09e063fe987a0dab8e0a1c3c
Dmitry Shishkin <4144442@bk.ru>
2022-08-02 12:03:38 +0300

    20. Valid Parentheses

--- src/SimpleTests.java
+++ src/SimpleTests.java
@@ -1,15 +1,10 @@
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.w3c.dom.NodeList;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class SimpleTests {
 
     private Solution solution;
-    private NodeList nodeList;
 
     @Before
     public void before() {
@@ -18,40 +13,82 @@ public class SimpleTests {
 
     @Test
     public void testSimple1() {
-        Solution.ListNode node1 = new Solution.ListNode(5, null);
-        Solution.ListNode node2 = new Solution.ListNode(4, node1);
-        Solution.ListNode node3 = new Solution.ListNode(3, node2);
-        Solution.ListNode node4 = new Solution.ListNode(2, node3);
-        Solution.ListNode node5 = new Solution.ListNode(1, node4);
-        Solution.ListNode ac = solution.removeNthFromEnd(node5, 2);
-        List<Integer> actual = new ArrayList<>();
-        while (ac.next != null) {
-            actual.add(ac.val);
-            ac = ac.next;
-        }
-        actual.add(ac.val);
-        List<Integer> expected = new ArrayList<>(List.of(1, 2, 3, 5));
+        boolean expected = true;
+        boolean actual = solution.isValid("()");
         Assert.assertEquals(expected, actual);
     }
-
     @Test
     public void testSimple2() {
-        Solution.ListNode node1 = new Solution.ListNode(1, null);
-        Solution.ListNode ac = solution.removeNthFromEnd(node1, 1);
-        Assert.assertNull(ac);
+        boolean expected = true;
+        boolean actual = solution.isValid("()[]{}");
+        Assert.assertEquals(expected, actual);
     }
     @Test
     public void testSimple3() {
-        Solution.ListNode node1 = new Solution.ListNode(2, null);
-        Solution.ListNode node2 = new Solution.ListNode(1, node1);
-        Solution.ListNode ac = solution.removeNthFromEnd(node2, 1);
-        List<Integer> actual = new ArrayList<>();
-        while (ac.next != null) {
-            actual.add(ac.val);
-            ac = ac.next;
-        }
-        actual.add(ac.val);
-        List<Integer> expected = new ArrayList<>(List.of(1));
+        boolean expected = false;
+        boolean actual = solution.isValid("(]");
         Assert.assertEquals(expected, actual);
     }
+    @Test
+    public void testSimple4() {
+        boolean expected = true;
+        boolean actual = solution.isValid("(({[]}))");
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSimple5() {
+        boolean expected = false;
+        boolean actual = solution.isValid("(({[]}){");
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSimple6() {
+        boolean expected = false;
+        boolean actual = solution.isValid("){");
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSimple7() {
+        boolean expected = true;
+        boolean actual = solution.isValid("{[]{()()}}");
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSimple8() {
+        boolean expected = false;
+        boolean actual = solution.isValid("{[]{(}()}}");
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSimple9() {
+        boolean expected = false;
+        boolean actual = solution.isValid("{[]{(}()})");
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSimple10() {
+        boolean expected = false;
+        boolean actual = solution.isValid("[([]])");
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSimple11() {
+        boolean expected = false;
+        boolean actual = solution.isValid("([([]]))");
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSimple12() {
+        boolean expected = false;
+        boolean actual = solution.isValid("(([(()))])");
+        Assert.assertEquals(expected, actual);
+    }
+    //Wrong Answer
+    //Details
+    //Input
+    //"[([]])"
+    //Output
+    //true
+    //Expected
+    //false
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,48 +1,28 @@
+import java.util.Stack;
 
 class Solution {
 
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head.next == null) return null;
-        ListNode tail = head;
-        for (int i = 1; i < n; i++) {
-            tail = tail.next;
+    public boolean isValid(String s) {
+        if (s.length()%2!=0)
+            return false;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') stack.push(ch);
+            else {
+                char sch;
+                if (!stack.empty()) sch = stack.pop();
+                else return false;
+                if ((sch == '(' && ch != ')') || (sch == '{' && ch != '}') || (sch == '[' && ch != ']'))
+                    return false;
+            }
         }
-        if (tail.next == null) {
-            head = head.next;
-            return head;
-        }
-        tail = tail.next;
-        ListNode delNode = head.next;
-        ListNode nextNode = delNode.next;
-        ListNode previousNode = head;
-        while (tail.next != null) {
-            tail = tail.next;
-            delNode = delNode.next;
-            previousNode = previousNode.next;
-            nextNode = nextNode.next;
-        }
-        previousNode.next = nextNode;
-        return head;
-    }
-
-    public static class ListNode {
-
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
+        return stack.empty();
     }
 }
+
+
+
 
 
 
