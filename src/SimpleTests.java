19adb00f0aec9b0b7b02324b27a03322a5c7bb8a
Dmitry Shishkin <4144442@bk.ru>
2022-08-04 10:13:15 +0300

    21. Merge Two Sorted Lists

--- src/SimpleTests.java
+++ src/SimpleTests.java
@@ -2,6 +2,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SimpleTests {
 
     private Solution solution;
@@ -13,82 +16,38 @@ public class SimpleTests {
 
     @Test
     public void testSimple1() {
-        boolean expected = true;
-        boolean actual = solution.isValid("()");
+        Solution.ListNode node1 = new Solution.ListNode(4);
+        Solution.ListNode node2 = new Solution.ListNode(2, node1);
+        Solution.ListNode node3 = new Solution.ListNode(1, node2);
+        Solution.ListNode node4 = new Solution.ListNode(4);
+        Solution.ListNode node5 = new Solution.ListNode(3, node4);
+        Solution.ListNode node6 = new Solution.ListNode(1, node5);
+        List<Integer> expected = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4));
+        List<Integer> actual = new ArrayList<>();
+        Solution.ListNode node = solution.mergeTwoLists(node3, node6);
+        while (node.next != null) {
+            actual.add(node.val);
+            node = node.next;
+        }
+        actual.add(node.val);
         Assert.assertEquals(expected, actual);
     }
+
     @Test
     public void testSimple2() {
-        boolean expected = true;
-        boolean actual = solution.isValid("()[]{}");
-        Assert.assertEquals(expected, actual);
+        Assert.assertNull(solution.mergeTwoLists(null, null));
     }
     @Test
     public void testSimple3() {
-        boolean expected = false;
-        boolean actual = solution.isValid("(]");
+        Solution.ListNode node1 = new Solution.ListNode();
+        List<Integer> expected = new ArrayList<>(List.of(0));
+        List<Integer> actual = new ArrayList<>();
+        Solution.ListNode node = solution.mergeTwoLists(null, node1);
+        while (node.next != null) {
+            actual.add(node.val);
+            node = node.next;
+        }
+        actual.add(node.val);
         Assert.assertEquals(expected, actual);
     }
-    @Test
-    public void testSimple4() {
-        boolean expected = true;
-        boolean actual = solution.isValid("(({[]}))");
-        Assert.assertEquals(expected, actual);
-    }
-    @Test
-    public void testSimple5() {
-        boolean expected = false;
-        boolean actual = solution.isValid("(({[]}){");
-        Assert.assertEquals(expected, actual);
-    }
-    @Test
-    public void testSimple6() {
-        boolean expected = false;
-        boolean actual = solution.isValid("){");
-        Assert.assertEquals(expected, actual);
-    }
-    @Test
-    public void testSimple7() {
-        boolean expected = true;
-        boolean actual = solution.isValid("{[]{()()}}");
-        Assert.assertEquals(expected, actual);
-    }
-    @Test
-    public void testSimple8() {
-        boolean expected = false;
-        boolean actual = solution.isValid("{[]{(}()}}");
-        Assert.assertEquals(expected, actual);
-    }
-    @Test
-    public void testSimple9() {
-        boolean expected = false;
-        boolean actual = solution.isValid("{[]{(}()})");
-        Assert.assertEquals(expected, actual);
-    }
-    @Test
-    public void testSimple10() {
-        boolean expected = false;
-        boolean actual = solution.isValid("[([]])");
-        Assert.assertEquals(expected, actual);
-    }
-    @Test
-    public void testSimple11() {
-        boolean expected = false;
-        boolean actual = solution.isValid("([([]]))");
-        Assert.assertEquals(expected, actual);
-    }
-    @Test
-    public void testSimple12() {
-        boolean expected = false;
-        boolean actual = solution.isValid("(([(()))])");
-        Assert.assertEquals(expected, actual);
-    }
-    //Wrong Answer
-    //Details
-    //Input
-    //"[([]])"
-    //Output
-    //true
-    //Expected
-    //false
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,45 @@
-import java.util.Stack;
 
 class Solution {
 
-    public boolean isValid(String s) {
-        if (s.length()%2!=0)
-            return false;
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == '(' || ch == '{' || ch == '[') stack.push(ch);
-            else {
-                char sch;
-                if (!stack.empty()) sch = stack.pop();
-                else return false;
-                if ((sch == '(' && ch != ')') || (sch == '{' && ch != '}') || (sch == '[' && ch != ']'))
-                    return false;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        ListNode listNode = new ListNode();
+        ListNode node = listNode;
+        while (list1 != null || list2 != null) {
+            if (list1 == null) {
+                listNode.next = list2;
+                list2 = list2.next;
+            } else if (list2 == null || list1.val <= list2.val) {
+                listNode.next = list1;
+                list1 = list1.next;
+            } else {
+                listNode.next = list2;
+                list2 = list2.next;
             }
+            listNode = listNode.next;
         }
-        return stack.empty();
+        return node.next;
+    }
+
+    public static class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 }
-
 
 
 
