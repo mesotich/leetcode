cf70e7f4e081c9981ba810f0997718943d981574
Dmitry Shishkin <4144442@bk.ru>
2022-08-16 21:44:43 +0300

    26. Remove Duplicates from Sorted Array

--- src/SimpleTests.java
+++ src/SimpleTests.java
@@ -2,9 +2,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class SimpleTests {
 
     private Solution solution;
@@ -16,42 +13,18 @@ public class SimpleTests {
 
     @Test
     public void testSimple1() {
-        Solution.ListNode node1 = new Solution.ListNode(5);
-        Solution.ListNode node2 = new Solution.ListNode(4, node1);
-        Solution.ListNode node3 = new Solution.ListNode(3, node2);
-        Solution.ListNode node4 = new Solution.ListNode(2, node3);
-        Solution.ListNode node5 = new Solution.ListNode(1, node4);
-        List<Integer> expected = new ArrayList<>(List.of(3, 2, 1, 4, 5));
-        Solution.ListNode node = solution.reverseKGroup(node5, 3);
-        List<Integer> actual = new ArrayList<>();
-        while (node != null) {
-            actual.add(node.val);
-            node = node.next;
-        }
-        Assert.assertEquals(expected, actual);
+        int actual = solution.removeDuplicates(new int[]{1, 1, 2});
+        Assert.assertEquals(2, actual);
     }
 
     @Test
     public void testSimple2() {
-        Solution.ListNode node1 = new Solution.ListNode(1);
-        Solution.ListNode actual = solution.reverseKGroup(node1, 1);
-        Assert.assertEquals(node1.val, actual.val);
+        int actual = solution.removeDuplicates(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4});
+        Assert.assertEquals(5, actual);
     }
-
     @Test
     public void testSimple3() {
-        Solution.ListNode node1 = new Solution.ListNode(5);
-        Solution.ListNode node2 = new Solution.ListNode(4, node1);
-        Solution.ListNode node3 = new Solution.ListNode(3, node2);
-        Solution.ListNode node4 = new Solution.ListNode(2, node3);
-        Solution.ListNode node5 = new Solution.ListNode(1, node4);
-        List<Integer> expected = new ArrayList<>(List.of(2, 1, 4, 3, 5));
-        Solution.ListNode node = solution.reverseKGroup(node5, 2);
-        List<Integer> actual = new ArrayList<>();
-        while (node != null) {
-            actual.add(node.val);
-            node = node.next;
-        }
-        Assert.assertEquals(expected, actual);
+        int actual = solution.removeDuplicates(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4,4,4});
+        Assert.assertEquals(5, actual);
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,49 +1,18 @@
 class Solution {
 
-    private ListNode[] arr;
-    private ListNode nextHead;
-
-    public ListNode reverseKGroup(ListNode head, int k) {
-        if (k == 1)
-            return head;
-        arr = new ListNode[k];
-        return recursive(head);
-    }
-
-    private ListNode recursive(ListNode head) {
-        if (head == null)
-            return null;
-        for (int i = 0; i < arr.length; i++) {
-            if (head == null)
-                return arr[0];
-            arr[i] = head;
-            head = head.next;
+    public int removeDuplicates(int[] nums) {
+        int start = 0;
+        int end = 1;
+        while (end < nums.length) {
+            while (nums[start] == nums[end]) {
+                end++;
+                if (end >= nums.length)
+                    return start + 1;
+            }
+            start++;
+            nums[start] = nums[end];
+            end++;
         }
-        int last = arr.length - 1;
-        ListNode lastNode = arr[last];
-        nextHead = arr[last].next;
-        for (int i = last; i >= 1; i--) {
-            arr[i].next = arr[i - 1];
-        }
-        arr[0].next = recursive(nextHead);
-        return lastNode;
-    }
-
-    public static class ListNode {
-
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
+        return start + 1;
     }
 }
