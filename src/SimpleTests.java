d6f70be399078e5e8b9419319059b4c745d8454e
Dmitry Shishkin <4144442@bk.ru>
2022-08-16 20:21:13 +0300

    25. Reverse Nodes in k-Group

--- src/SimpleTests.java
+++ src/SimpleTests.java
@@ -16,13 +16,13 @@ public class SimpleTests {
 
     @Test
     public void testSimple1() {
-        Solution.ListNode node1 = new Solution.ListNode(4);
-        Solution.ListNode node2 = new Solution.ListNode(3, node1);
-        Solution.ListNode node3 = new Solution.ListNode(2, node2);
-        Solution.ListNode node4 = new Solution.ListNode(1,node3);
-
-        List<Integer> expected = new ArrayList<>(List.of(2,1,4,3));
-        Solution.ListNode node = solution.swapPairs(node4);
+        Solution.ListNode node1 = new Solution.ListNode(5);
+        Solution.ListNode node2 = new Solution.ListNode(4, node1);
+        Solution.ListNode node3 = new Solution.ListNode(3, node2);
+        Solution.ListNode node4 = new Solution.ListNode(2, node3);
+        Solution.ListNode node5 = new Solution.ListNode(1, node4);
+        List<Integer> expected = new ArrayList<>(List.of(3, 2, 1, 4, 5));
+        Solution.ListNode node = solution.reverseKGroup(node5, 3);
         List<Integer> actual = new ArrayList<>();
         while (node != null) {
             actual.add(node.val);
@@ -33,14 +33,25 @@ public class SimpleTests {
 
     @Test
     public void testSimple2() {
-        Solution.ListNode actual = solution.swapPairs(null);
-        Assert.assertNull(actual);
-    }
-    @Test
-    public void testSimple3() {
         Solution.ListNode node1 = new Solution.ListNode(1);
-        Solution.ListNode actual = solution.swapPairs(node1);
-        Assert.assertEquals(node1.val,actual.val);
+        Solution.ListNode actual = solution.reverseKGroup(node1, 1);
+        Assert.assertEquals(node1.val, actual.val);
     }
 
+    @Test
+    public void testSimple3() {
+        Solution.ListNode node1 = new Solution.ListNode(5);
+        Solution.ListNode node2 = new Solution.ListNode(4, node1);
+        Solution.ListNode node3 = new Solution.ListNode(3, node2);
+        Solution.ListNode node4 = new Solution.ListNode(2, node3);
+        Solution.ListNode node5 = new Solution.ListNode(1, node4);
+        List<Integer> expected = new ArrayList<>(List.of(2, 1, 4, 3, 5));
+        Solution.ListNode node = solution.reverseKGroup(node5, 2);
+        List<Integer> actual = new ArrayList<>();
+        while (node != null) {
+            actual.add(node.val);
+            node = node.next;
+        }
+        Assert.assertEquals(expected, actual);
+    }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,32 @@
 class Solution {
 
-    public ListNode swapPairs(ListNode head) {
+    private ListNode[] arr;
+    private ListNode nextHead;
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (k == 1)
+            return head;
+        arr = new ListNode[k];
+        return recursive(head);
+    }
+
+    private ListNode recursive(ListNode head) {
         if (head == null)
             return null;
-        if (head.next == null)
-            return head;
-        ListNode next = head.next.next;
-        ListNode swap = head.next;
-        swap.next = head;
-        head.next = swapPairs(next);
-        return swap;
+        for (int i = 0; i < arr.length; i++) {
+            if (head == null)
+                return arr[0];
+            arr[i] = head;
+            head = head.next;
+        }
+        int last = arr.length - 1;
+        ListNode lastNode = arr[last];
+        nextHead = arr[last].next;
+        for (int i = last; i >= 1; i--) {
+            arr[i].next = arr[i - 1];
+        }
+        arr[0].next = recursive(nextHead);
+        return lastNode;
     }
 
     public static class ListNode {
