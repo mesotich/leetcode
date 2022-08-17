cd3774933b9f9d084b70f3c8757e5eca0dacbe23
Dmitry Shishkin <4144442@bk.ru>
2022-08-17 12:16:58 +0300

    28. Implement strStr()

--- src/SimpleTests.java
+++ src/SimpleTests.java
@@ -13,23 +13,54 @@ public class SimpleTests {
 
     @Test
     public void testSimple1() {
-        int actual = solution.removeElement(new int[]{3, 2, 2, 3}, 3);
+        int actual = solution.strStr("hello", "ll");
         Assert.assertEquals(2, actual);
     }
-
     @Test
     public void testSimple2() {
-        int actual = solution.removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2);
-        Assert.assertEquals(5, actual);
+        int actual = solution.strStr("aaaaa", "bba");
+        Assert.assertEquals(-1, actual);
     }
     @Test
     public void testSimple3() {
-        int actual = solution.removeElement(new int[]{2,2,2,2}, 2);
+        int actual = solution.strStr("aaaaa", "a");
         Assert.assertEquals(0, actual);
     }
     @Test
     public void testSimple4() {
-        int actual = solution.removeElement(new int[]{2,2,2,2,3,3}, 3);
+        int actual = solution.strStr("hello", "llo");
+        Assert.assertEquals(2, actual);
+    }
+    @Test
+    public void testSimple5() {
+        int actual = solution.strStr("hello", "hely");
+        Assert.assertEquals(-1, actual);
+    }
+    @Test
+    public void testSimple6() {
+        int actual = solution.strStr("a", "a");
+        Assert.assertEquals(0, actual);
+    }
+    @Test
+    public void testSimple7() {
+        int actual = solution.strStr("a", "a");
+        Assert.assertEquals(0, actual);
+    }
+    @Test
+    public void testSimple8() {
+        int actual = solution.strStr("wer", "a");
+        Assert.assertEquals(-1, actual);
+    }
+    @Test
+    public void testSimple9() {
+        int actual = solution.strStr("mississippi", "issip");
         Assert.assertEquals(4, actual);
     }
+    @Test
+    public void testSimple10() {
+        int actual = solution.strStr("abc", "c");
+        Assert.assertEquals(2, actual);
+    }
+
+
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,27 @@
 class Solution {
 
-    public int removeElement(int[] nums, int val) {
-        int start = 0;
-        int end = 0;
-        while (end < nums.length) {
-            while (nums[end] == val) {
-                end++;
-                if (end >= nums.length)
-                    return start;
+    public int strStr(String haystack, String needle) {
+        int st = 0;
+        char ch = needle.charAt(0);
+        while (st + needle.length() <= haystack.length()) {
+            if (haystack.charAt(st) != ch) {
+                st++;
+                continue;
             }
-            nums[start] = nums[end];
-            start++;
-            end++;
+            if (needle.length() == 1)
+                return st;
+            if (st > haystack.length() - 2)
+                return -1;
+            int n = 1;
+            while (haystack.charAt(st + n) == needle.charAt(n)) {
+                if (n == needle.length() - 1)
+                    return st;
+                if (st + n == haystack.length() - 1)
+                    return -1;
+                n++;
+            }
+            st++;
         }
-        return start;
+        return -1;
     }
 }
